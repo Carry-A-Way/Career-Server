da7a17f85ac3eb2b4e61aed2dad431ad7f216d8d
JaejunHan <hanjj03@naver.com>
2023-09-06 19:25:40 +0900

    FIX: Erase fields in TutorDetail table - major1, 2, 3, and career field
    Due to existence of new Tables "School" and "Career"

--- src/main/java/com/example/career/domain/user/Entity/TutorDetail.java
+++ src/main/java/com/example/career/domain/user/Entity/TutorDetail.java
@@ -22,15 +22,6 @@ public class TutorDetail {
 //    @OneToOne(cascade = CascadeType.ALL)
 //    @JoinColumn(name = "tutor_id", referencedColumnName = "id")
 //    private Long user; // user
-    
-    @Column(columnDefinition = "VARCHAR(15)", nullable = false)
-    private String major1;
-
-    @Column(columnDefinition = "VARCHAR(15)")
-    private String major2;
-
-    @Column(columnDefinition = "VARCHAR(15)")
-    private String major3;
 
     @Column(columnDefinition = "VARCHAR(15)", nullable = false)
     private String consultingMajor1;
@@ -40,9 +31,6 @@ public class TutorDetail {
 
     @Column(columnDefinition = "VARCHAR(15)")
     private String consultingMajor3;
-
-    @Column(columnDefinition = "JSON")
-    private String career;
 
     @Column(nullable = false)
     private int cash = 0;
