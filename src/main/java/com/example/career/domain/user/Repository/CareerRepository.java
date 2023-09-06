e3f0b894239a6177ff211b171a757f6141c0d8e3
JaejunHan <hanjj03@naver.com>
2023-09-07 00:31:58 +0900

    FEAT: findByTutorId method in Career, Tag, School repositories

--- src/main/java/com/example/career/domain/user/Repository/CareerRepository.java
+++ src/main/java/com/example/career/domain/user/Repository/CareerRepository.java
@@ -5,9 +5,11 @@ import com.example.career.domain.user.Entity.School;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CareerRepository extends JpaRepository<Career, Long> {
     public Optional<Career> findByTutor_idAndIdx(Long id, Long idx);
+    public List<Career> findByTutor_id(Long id);
 }
--- src/main/java/com/example/career/domain/user/Repository/TagRepository.java
+++ src/main/java/com/example/career/domain/user/Repository/TagRepository.java
@@ -5,9 +5,12 @@ import com.example.career.domain.user.Entity.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface TagRepository extends JpaRepository<Tag,Long> {
     public Optional<Tag> findByTutor_idAndIdx(Long id, Long idx);
+
+    public List<Tag> findByTutor_id(Long id);
 }
