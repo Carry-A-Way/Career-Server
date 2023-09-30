d0eb4d6cc7942632c20a2d64325f4053ec6e03a9
JaejunHan <hanjj03@naver.com>
2023-10-01 01:13:15 +0900

    FEAT: Add Images handler to Article Controller, Service, and Repository
    사진을 고려하여 Article과 관련된 컨트롤러, 서비스, 리포지토리 수정

--- src/main/java/com/example/career/domain/community/Repository/ArticleRepository.java
+++ src/main/java/com/example/career/domain/community/Repository/ArticleRepository.java
@@ -19,12 +19,6 @@ public interface ArticleRepository extends JpaRepository<Article, Long> {
 
     @Modifying
     @Transactional
-    @Query("UPDATE Article a SET a.title = :title, a.content = :content, a.updatedAt = :updatedAt WHERE a.id = :id AND a.userId = :userId")
-    void updateArticleTitleAndContent(@Param("userId") Long userId, @Param("id") Long id, @Param("title") String title, @Param("content") String content,  @Param("updatedAt") LocalDateTime updatedAt);
-
-
-    @Modifying
-    @Transactional
     @Query("UPDATE Article a SET a.heartCnt = a.heartCnt + 1 WHERE a.id = :id AND a.userId = :userId")
     void incrementArticleThumbsUp(@Param("id") Long id, @Param("userId") Long userId);
 
