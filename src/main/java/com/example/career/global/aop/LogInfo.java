bd1ddc2629083dfeaa2748f3a8c7914b180f59ae
JONGMIN SHIN <jongmin373@gmail.com>
2024-01-30 10:37:54 +0900

    FEAT : create Logging System
    API 호출에 대한 로깅 시스템을 구축하고 있습니다.

--- src/main/java/com/example/career/global/aop/LogInfo.java
+++ src/main/java/com/example/career/global/aop/LogInfo.java
@@ -1,2 +1,34 @@
-package com.example.career.global.aop;public class LogInfo {
+package com.example.career.global.aop;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LogInfo {
+    private String url;
+    private String name;
+    private String method;
+//    private String header;
+    private String parameters;
+    private String body;
+    private String ipAddress;
+//    private String userId;
+    private String username;
+    private String exception; // 예외 정보
+
+    public LogInfo(String url, String name, String method, String parameters, String body, String ipAddress, String userName) {
+        this.url = url;
+        this.name = name;
+        this.method = method;
+//        this.header = header;
+        this.parameters = parameters;
+        this.body = body;
+        this.ipAddress = ipAddress;
+//        this.userId = userId;
+        this.username = userName;
+    }
+
 }
--- src/main/java/com/example/career/global/aop/LoggingAspect.java
+++ src/main/java/com/example/career/global/aop/LoggingAspect.java
@@ -1,2 +1,74 @@
-package com.example.career.global.aop;public class LoggingAspect {
+package com.example.career.global.aop;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Map;
+
+@Aspect
+@Component
+@RequiredArgsConstructor
+public class LoggingAspect {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
+    private final ObjectMapper objectMapper;
+
+    @Pointcut("within(*..*Controller)")
+    public void onRequest() {}
+
+    @Around("onRequest()")
+    public Object requestLogging(ProceedingJoinPoint joinPoint) throws Throwable {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String userId = null;
+        String username = null;
+
+        if (authentication != null && authentication.getPrincipal() instanceof UserDetails) {
+            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+            username = userDetails.getUsername(); // 유저 ID 또는 username
+            // userName = [유저 이름을 가져오는 로직]; // 필요한 경우 유저 이름을 설정합니다.
+        }
+        System.out.println(username);
+        final RequestApiInfo apiInfo = new RequestApiInfo(joinPoint, joinPoint.getTarget().getClass(), objectMapper);
+
+        final LogInfo logInfo = new LogInfo(
+                apiInfo.getUrl(),
+                apiInfo.getName(),
+                apiInfo.getMethod(),
+                objectMapper.writeValueAsString(apiInfo.getParameters()),
+                objectMapper.writeValueAsString(apiInfo.getBody()),
+                apiInfo.getIpAddress(),
+                username // 여기에 userName 추가
+        );
+        System.out.println(apiInfo);
+        System.out.println(logInfo);
+
+        try {
+            final Object result = joinPoint.proceed(joinPoint.getArgs());
+            final String logMessage = objectMapper.writeValueAsString(Map.entry("logInfo", logInfo));
+            logger.info(logMessage);
+
+            System.out.println("test22");
+            return result;
+        } catch (Exception e) {
+            final StringWriter sw = new StringWriter();
+            e.printStackTrace(new PrintWriter(sw));
+            final String exceptionAsString = sw.toString();
+            logInfo.setException(exceptionAsString);
+            final String logMessage = objectMapper.writeValueAsString(logInfo);
+            logger.error(logMessage);System.out.println("test333");
+            throw e;
+        }
+    }
 }
