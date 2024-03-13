1c3f7de0b7d19050cd034922e079a91fd840e993
JaejunHan <hanjj03@naver.com>
2024-03-14 03:35:33 +0900

    FEAT: Implement KAKAO SignUp & SignIn
    카카오톡을 이용한 회원 가입 및 로그인 구현

--- src/main/java/com/example/career/domain/oauth/Service/UserSnsService.java
+++ src/main/java/com/example/career/domain/oauth/Service/UserSnsService.java
@@ -1,2 +1,11 @@
-package com.example.career.domain.oauth.Service;public interface UserSnsService {
+package com.example.career.domain.oauth.Service;
+
+import com.example.career.domain.oauth.Entity.UserSns;
+import org.springframework.transaction.annotation.Transactional;
+
+public interface UserSnsService {
+    @Transactional
+    void snsSignup(Long userId, int type, Long snsId);
+
+    UserSns findBySnsId(Long snsId);
 }
